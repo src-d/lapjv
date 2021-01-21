171a799c68c2c5479c2c85b8531e15624f322abc
Vadim Markovtsev <gmarkhor@gmail.com>
2021-01-21 16:14:42 +0000

    Bump version: 1.3.1 → 1.3.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="Linear sum assignment problem solver using Jonker-Volgenant algorithm.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="1.3.1",
+    version="1.3.2",
     license="MIT",
     author="Vadim Markovtsev",
     author_email="gmarkhor@gmail.com",
